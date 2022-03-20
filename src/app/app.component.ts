84b076cf1a3c7e702951098a444c85e0bd5f5ab5
Peyman <coding.peyman.22@gmail.com>
2022-03-20 16:12:01 +0330

    Lesson 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,189 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'angular-app';
+// }
+
+/******************************************************/
+
+// import { Component } from "@angular/core";
+
+// @Component({
+//   selector:'app-root',
+//   template:'<h1>Hello World !</h1>',
+// })
+// export class AppComponent{
+//   title = 'angular-app'
+// }
+
+/******************************************************/
+
+// import { Component } from "@angular/core";
+
+// @Component({
+//   selector:'app-root',
+//   template:'<h1>Hello World !</h1>',
+//   styles:[
+//     'h1 {text-align: center;}',
+//     'h1 {color:red;}',
+//   ]
+// })
+// export class AppComponent{
+//   title = 'angular-app'
+// }
+
+/******************************************************/
+
+// import { Component } from "@angular/core";
+
+// @Component({
+//   selector:'app-root',
+//   templateUrl:'./app.component.html',
+//   styleUrls:['./app.component.css'],
+// })
+// export class AppComponent{
+//   title = 'angular-app'; //How Compiler Write => public title : string  = 'angular-app'
+//   public constructor(){
+//     Last initial And Value
+//     this.title = `test`;
+//   }
+// }
+
+/******************************************************/
+
+//For Use ngModel => Should Be Write Folowing Codes In app.module.ts
+
+//import { FormsModule } from '@angular/forms';
+//imports: [
+//  FormsModule,
+//  ...,
+//  ..,
+//],
+
+// import { Component } from "@angular/core";
+
+// @Component({
+//   selector:'app-root',
+//  template:`
+//     <label>Enter Your Text : </label>
+//     <input type="text" [(ngModel)]="title">
+//     <h1>{{ title }}</h1>
+//  `,
+// })
+// export class AppComponent{
+//   title = ""
+// }
+
+/******************************************************/
+
+// import { Component } from "@angular/core";
+
+// @Component({
+//   selector:'app-root',
+//   templateUrl:'./app.component.html',
+//   styleUrls:['./app.component.css',],
+// })
+// export class AppComponent{
+//   constructor() {
+//     this.title = "Preview"
+//   }
+//   public title:string; //should be Init (In Constractor)
+// }
+
+/******************************************************/
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   template: `
+//     <ul>
+//         <li *ngFor="let item of Items">{{ item }}</li>
+//     </ul>
+// `,
+// })
+// export class AppComponent {
+//   constructor() {
+//     this.title = "Perview";
+//sign a Field
+//     this.Items =[];
+//       for (let index = 1; index <= 10; index++) {
+//         this.Items.push(`Person` + index);
+//       }
+//     }
+//   public title:string;
+//   public Items:string[];
+// }
+
+/******************************************************/
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   template: `
+//     <label>Display</label>
+//     <input type="checkbox" [(ngModel)]="display">
+//     <ul *ngIf="display">
+//         <li *ngFor="let item of Items">{{ item }}</li>
+//     </ul>
+// `,
+// })
+// export class AppComponent {
+//   constructor() {
+//     this.display = true;
+//     this.Items = [];
+//     this.title = 'Perview';
+//     for (let index = 1; index <= 10; index++) {
+//       this.Items.push(`Person` + index);
+//     }
+//   }
+//   public title: string;
+//   public Items: string[];
+//   public display: boolean;
+// }
+
+/******************************************************/
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   template: `
+//     <label>Display</label>
+//     <input type="checkbox" [(ngModel)]="!display">
+//     <ul [hidden]="display">
+//         <li *ngFor="let item of Items">{{ item }}</li>
+//     </ul>
+// `,
+// })
+// export class AppComponent {
+//   constructor() {
+//     this.display = true;
+//     this.Items = [];
+//     this.title = 'Perview';
+//     for (let index = 1; index <= 10; index++) {
+//       this.Items.push(`Person` + index);
+//     }
+//   }
+//   public title: string;
+//   public Items: string[];
+//   public display: boolean;
+// }
+
+/******************************************************/
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: ` <h1>Lesson 2 First Code</h1> `,
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'Lesson 2';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,11 +7,12 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
